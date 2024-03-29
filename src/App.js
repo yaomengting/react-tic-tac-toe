8695fe1ac74baf1a59c3fe45c61cc50bab51a969
Mengting Yao <mengtingyao@Mengtings-Mac-mini.local>
2024-03-29 14:24:10 -0500

    implement check winner function

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import './App.css';
 import Board from './components/Board';
 import {useState} from 'react';
 function App() {
+  const WIN_CONDITIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],]
+  
 
   const [board, setBoard] = useState(Array(9).fill(null));
   const [xPlaying, setXPlaying] = useState(true);
@@ -14,8 +24,18 @@ function App() {
         return value;
       }
     })
+    checkWinner(updatedBoard)
     setBoard(updatedBoard);
     setXPlaying(!xPlaying)
+  }
+
+  const checkWinner = (board) => {
+    for(let i = 0; i < WIN_CONDITIONS.length; i++){
+      const [x, y, z] = WIN_CONDITIONS[i];
+      if(board[x] && board[x] === board[y]&& board[y] === board[z]){
+        return board[x];
+      }
+    }
   }
   return (
     <div className="App">

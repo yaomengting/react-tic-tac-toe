c4d4ff5d2ac8c1a692d3c944283508aa4c74bd80
Mengting Yao <mengtingyao@Mengtings-Mac-mini.local>
2024-03-29 14:28:16 -0500

    show winner

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
 
   const [board, setBoard] = useState(Array(9).fill(null));
   const [xPlaying, setXPlaying] = useState(true);
+  const [winner, setWinner]= useState(null);
   const handleBoxClick = (boxIdx) => {
     const updatedBoard = board.map((value, idx) => {
       if(idx === boxIdx){
@@ -24,7 +25,8 @@ function App() {
         return value;
       }
     })
-    checkWinner(updatedBoard)
+    const winner = checkWinner(updatedBoard);
+    if (winner) setWinner(winner); 
     setBoard(updatedBoard);
     setXPlaying(!xPlaying)
   }
@@ -39,6 +41,7 @@ function App() {
   }
   return (
     <div className="App">
+      <div>Winner is:  {winner}</div>
    <Board board={board} onClick={handleBoxClick}/>
     </div>
   );

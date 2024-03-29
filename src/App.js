dc1ffa4195de22c1fa0099632579e5a4a5fedfeb
Mengting Yao <mengtingyao@Mengtings-Mac-mini.local>
2024-03-29 14:20:23 -0500

    render X and O in turn

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import {useState} from 'react';
 function App() {
 
   const [board, setBoard] = useState(Array(9).fill(null));
+  const [xPlaying, setXPlaying] = useState(true);
   const handleBoxClick = (boxIdx) => {
     const updatedBoard = board.map((value, idx) => {
       if(idx === boxIdx){
-        return 'X';
+        return xPlaying === true ? "X": "O";
       }else {
         return value;
       }
     })
     setBoard(updatedBoard);
+    setXPlaying(!xPlaying)
   }
   return (
     <div className="App">
